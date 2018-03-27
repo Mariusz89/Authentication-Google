be22457b4ee02c1cd7a3f617948abf4cd83b2caa
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2018-03-27 19:29:10 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '39861560199-c9lb87dobmi5i0cvkckdkrj6pakvvsp4.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'E6g1A4ke_bLycynXGzFuEma1',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
